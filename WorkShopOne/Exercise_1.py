b1874412b75ab7944e2cd634ce7cd2d4474b0d12
Charlie Roadhouse <62378859+chroadhouse@users.noreply.github.com>
2023-04-06 17:11:21 +0100

    Made Separate folders for the workshops
    Code added for some of the tasks, with the main bulk of day one workshop done

---
+++ WorkShopOne/Exercise_1.py
@@ -0,0 +1,14 @@
+# Now you know about the different data types
+# Try storing the appropriate type for each variable 
+# Once you have completed the code, why not run it and see what happens ? 
+
+
+name = 
+
+age = 
+
+# Something for double 
+
+# Something for Boolean
+
+print(f'Hello my name is {name} and I am {age} years old. ')
