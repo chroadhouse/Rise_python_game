38c0c5b7797774d92bb17d0bc2688f13650fae8b
Charlie Roadhouse <62378859+chroadhouse@users.noreply.github.com>
2023-04-18 19:57:00 +0100

    smalll changes

--- WorkShopTwo/Exercise_1.py
+++ WorkShopTwo/Exercise_1.py
@@ -1,2 +1,18 @@
 # If statements 
 
+# We are going to build a menu for our new naughts and crosses game
+
+while True:
+    print('Welcome to Naughts and Crosses')
+    print('1. Play one player')
+    print('2. Print play two player')
+    print('3. Settings')
+    print('4 Exit')
+    userInput = input('Please write your number here')
+
+    # Add elif statements for the rest of the options - printing a message and using the break keywords for exit
+    
+
+    if userInput=='1':
+        print('Now playing one player')
+    
